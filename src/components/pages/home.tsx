c7ca47c0deeec597ca9460c69e6562939371d79b
Caio <pess.caio@hotmail.com>
2026-03-05 10:59:49 -0300

    delete page home

--- src/components/pages/home.tsx
+++
@@ -1,8 +0,0 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NavMenu } from "../nav-menu";
-import { ExpirationsPage } from "./Expirations";
-import { ProductsPage } from "./Products";
-
-export function Home() {
-  return <h1>teste</h1>;
-}
